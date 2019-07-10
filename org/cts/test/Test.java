b39e740eadd523e5b7b37781664c85991fca6dac
ktrinath <kajekarthi@gmail.com>
2019-07-10 19:04:20 +0530

    second

---
+++ org/cts/test/Test.java
@@ -0,0 +1,18 @@
+package org.cts.test;
+
+public class Test {
+	
+	public void Employee() {
+		System.out.println("employee name");
+	}
+	public void Employeeid() {
+		System.out.println("employee id");
+
+	}
+	public static void main(String[] args) {
+		Test t= new Test();
+		t.Employee();
+		t.Employeeid();
+	}
+
+}
